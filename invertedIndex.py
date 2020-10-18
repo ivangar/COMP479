8f33ff4b30c44d570df8e33f7c7fe57092a02ce9
ivan <garzon861@gmail.com>
2020-10-17 21:04:05 -0400

    Refactored inverted index class

--- invertedIndex.py
+++ invertedIndex.py
@@ -1,47 +1,10 @@
 from nltk import word_tokenize
 from bs4 import BeautifulSoup
 from nltk.corpus import PlaintextCorpusReader
-from nltk.corpus import stopwords
-import nltk, re, pprint
-import json
+import re
 import string
-import argparse
 import json
-import sys
-from os import path
 import itertools
-
-#Use this helper function to test a small amount of data
-def test_token_list():
-
-    # load reuters files with the help of NLTK's PlaintextCorpusReader
-    sgm_files = PlaintextCorpusReader("reuters", '.*\.sgm')
-    sgm_fileIds = sgm_files.fileids() #TO REMOVE LATER
-    token_list = []
-
-    f = open("reuters/reut2-000.sgm")
-    sgm_file = f.read()
-    parsed_sgm = BeautifulSoup(sgm_file, 'html.parser')
-    unsorted_token_list = open("unsorted_token_list.txt", 'w') #use for pretty print the list
-
-    for document_text in parsed_sgm.find_all('reuters'):
-        doc_id = int(document_text['newid'])
-        if doc_id > 4:
-            continue
-        doc_text = str(document_text.find('text'))
-        raw = BeautifulSoup(doc_text, 'html.parser').get_text()
-        raw = raw.replace("\u0002", '')
-        raw = raw.replace("\u0003", '')
-        for c in string.punctuation:
-            raw = raw.replace(c, " ")
-        raw = re.sub(r"\d", "", raw)
-        tokens = word_tokenize(raw)
-        for token in tokens:
-            token_list.append((token, doc_id))
-            print(json.dumps([token, doc_id]), file=unsorted_token_list)
-
-    with open("unsorted_token_list.json", mode="w", encoding="utf-8") as myFile:
-        json.dump(token_list, myFile)
 
 
 def generate_token_list():
@@ -57,38 +20,53 @@ def generate_token_list():
         parsed_sgm = BeautifulSoup(sgm_file, 'html.parser')
 
         for document_text in parsed_sgm.find_all('reuters'):
-            doc_id = int(document_text['newid'])
-            doc_text = str(document_text.find('text'))
-            raw = BeautifulSoup(doc_text, 'html.parser').get_text()
-            raw = raw.replace("\u0002", '')
-            raw = raw.replace("\u0003", '')
-            for c in string.punctuation:
-                raw = raw.replace(c, " ")
-            tokens = word_tokenize(raw)
-            for token in tokens:
-                token_list.append((token, doc_id))
-                print(json.dumps([token, doc_id]), file=unsorted_token_list)
+            block_tokenizer(document_text, token_list, unsorted_token_list)
 
     with open("unsorted_token_list.json", mode="w", encoding="utf-8") as myFile:
         json.dump(token_list, myFile)
 
 
-def sort_unique(unsorted_list):
+def block_tokenizer(document_text, token_list, unsorted_token_list):
+
+    doc_id = int(document_text['newid'])
+    doc_text = str(document_text.find('text'))
+    raw = BeautifulSoup(doc_text, 'html.parser').get_text()
+    raw = raw.replace("\u0002", '')
+    raw = raw.replace("\u0003", '')
+    for c in string.punctuation:
+        raw = raw.replace(c, " ")
+    tokens = word_tokenize(raw)
+    for token in tokens:
+        token_list.append((token, doc_id))
+        print(json.dumps([token, doc_id]), file=unsorted_token_list)
+
+
+def sort_tokens(unsorted_list):
 
     f = open(unsorted_list)
     file = f.read()
     res = json.loads(file)
-    unique_tokens = []
     sorted_list = sorted(res, key=lambda x: x[0])
-    term = sorted_list[0][0]
-    doc_id = sorted_list[0][1]
+
+    with open("sorted_tokens.json", mode="w", encoding="utf-8") as myFile:
+        json.dump(sorted_list, myFile)
+
+
+def remove_duplicates(sorted_list):
+
+    f = open(sorted_list)
+    file = f.read()
+    sorted_list_res = json.loads(file)
+    unique_tokens = []
+    term = sorted_list_res[0][0]
+    doc_id = sorted_list_res[0][1]
     unique_tokens.append((term, doc_id))
 
     #print to txt to pretty print
     unique_list = open("sorted_unique_tokens.txt", 'w')
     print(json.dumps([term, doc_id]), file=unique_list)
 
-    for token in sorted_list:
+    for token in sorted_list_res:
         if token[0] == term:
             if token[1] == doc_id:
                 continue
@@ -137,10 +115,45 @@ def generate_posting_list(sorted_list):
         json.dump(naive_indexer, myFile)
 
 
-#test_token_list()
-#generate_token_list()
+#Use this helper function to test a small amount of data
+def test_token_list():
 
-#sort_unique("unsorted_token_list.json")
+    # load reuters files with the help of NLTK's PlaintextCorpusReader
+    sgm_files = PlaintextCorpusReader("reuters", '.*\.sgm')
+    sgm_fileIds = sgm_files.fileids() #TO REMOVE LATER
+    token_list = []
+
+    f = open("reuters/reut2-000.sgm")
+    sgm_file = f.read()
+    parsed_sgm = BeautifulSoup(sgm_file, 'html.parser')
+    unsorted_token_list = open("unsorted_token_list.txt", 'w') #use for pretty print the list
+
+    for document_text in parsed_sgm.find_all('reuters'):
+        doc_id = int(document_text['newid'])
+        if doc_id > 4:
+            continue
+        doc_text = str(document_text.find('text'))
+        raw = BeautifulSoup(doc_text, 'html.parser').get_text()
+        raw = raw.replace("\u0002", '')
+        raw = raw.replace("\u0003", '')
+        for c in string.punctuation:
+            raw = raw.replace(c, " ")
+        raw = re.sub(r"\d", "", raw)
+        tokens = word_tokenize(raw)
+        for token in tokens:
+            token_list.append((token, doc_id))
+            print(json.dumps([token, doc_id]), file=unsorted_token_list)
+
+    with open("unsorted_token_list.json", mode="w", encoding="utf-8") as myFile:
+        json.dump(token_list, myFile)
+
+
+generate_token_list()
+
+sort_tokens("unsorted_token_list.json")
+
+remove_duplicates("sorted_tokens.json")
 
 generate_posting_list("sorted_unique_tokens.json")
 
+#test_token_list()
